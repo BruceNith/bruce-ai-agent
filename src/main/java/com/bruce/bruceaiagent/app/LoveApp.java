f7f609afbf27e22b71915695a96dbf4dca7bce76
BruceNith <nitianhua@outlook.com>
2025-09-18 15:40:45 +0800

    第三次提交: RAG 知识库基础
    1. 理解 RAG 工作流，在阿里百炼平台上创建知识库并分割文档
    2. 基于 Spring AI + 本地知识库实现 RAG
    3. 基于 Spring AI + 云知识库实现 RAG

--- src/main/java/com/bruce/bruceaiagent/app/LoveApp.java
+++ src/main/java/com/bruce/bruceaiagent/app/LoveApp.java
@@ -3,13 +3,17 @@ package com.bruce.bruceaiagent.app;
 import com.bruce.bruceaiagent.advisor.MyLoggerAdvisor;
 import com.bruce.bruceaiagent.advisor.ReReadingAdvisor;
 import com.bruce.bruceaiagent.chatMemory.FileBasedChatMemory;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -27,6 +31,12 @@ public class LoveApp {
             "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
             "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
             "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
+
+    @Resource
+    private VectorStore loveAppVectorStore;
+
+    @Resource
+    private Advisor loveAppRagCloudAdvisor;
 
     /**
      * 创建基于Dashscope的ChatClient实例
@@ -95,4 +105,30 @@ public class LoveApp {
         return loveReport;
     }
 
+    /**
+     * 和 RAG 知识库进行对话
+     * 第一种方式：基于内存
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChatWithRag(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                // 开启日志，便于观察效果
+                .advisors(new MyLoggerAdvisor())
+                // 应用 RAG 知识库问答
+//                .advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
+                // 应用 RAG 检索增强服务 （基于云知识库服务）
+                .advisors(loveAppRagCloudAdvisor)
+                .call()
+
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
 }
